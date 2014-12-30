66ea10baa0663c2dd7ac33a854ed8d81c7b2091f
Chicken-Bones <mehvids@gmail.com>
2014-12-30 19:41:48 +1000

    Link mod versions

--- src/codechicken/core/asm/CodeChickenCoreModContainer.java
+++ src/codechicken/core/asm/CodeChickenCoreModContainer.java
@@ -38,11 +38,11 @@ public class CodeChickenCoreModContainer extends DummyModContainer
     public List<ArtifactVersion> getDependants() {
         LinkedList<ArtifactVersion> deps = new LinkedList<ArtifactVersion>();
         if(!getVersion().contains("$")) {
-            deps.add(VersionParser.parseVersionReference("NotEnoughItems@[1.0.3,)"));
-            deps.add(VersionParser.parseVersionReference("EnderStorage@[1.4.5,)"));
-            deps.add(VersionParser.parseVersionReference("ChickenChunks@[1.3.4,)"));
-            deps.add(VersionParser.parseVersionReference("Translocator@[1.1.1,)"));
-            deps.add(VersionParser.parseVersionReference("WR-CBE|Core@[1.4.1,)"));
+            deps.add(VersionParser.parseVersionReference("NotEnoughItems@[1.0.4,)"));
+            deps.add(VersionParser.parseVersionReference("EnderStorage@[1.4.6,)"));
+            deps.add(VersionParser.parseVersionReference("ChickenChunks@[1.3.5,)"));
+            deps.add(VersionParser.parseVersionReference("Translocator@[1.1.2,)"));
+            deps.add(VersionParser.parseVersionReference("WR-CBE|Core@[1.4.2,)"));
         }
         return deps;
     }
